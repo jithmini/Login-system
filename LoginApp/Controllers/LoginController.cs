982e07d505bf5d27c559ed4020c1a83fded42a52
Jithmini <jith4ever@gmail.com>
2020-02-12 15:17:25 +0530

    commited

---
+++ LoginApp/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using LoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(LoginApp.Models.User userModel)
+        {
+            using (LoginDatabaseEntities db = new LoginDatabaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage="Wrong user name or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                    Session["UserName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+                
+        }
+
+        public ActionResult Logout()
+        {
+            int userID = (int)Session["UserID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
